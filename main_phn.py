e85f3346d6b707be2ca05ae05adfbdd82a03f3c7
gemsanyu <git.gemilang.santiyuda@gmail.com>
2022-08-09 23:02:48 +0800

    add epoch tqdm

--- main_phn.py
+++ main_phn.py
@@ -95,7 +95,7 @@ def run(args):
     num_items_per_city_list = [1,3,5]
     config_list = [(num_nodes, num_items_per_city) for num_nodes in num_nodes_list for num_items_per_city in num_items_per_city_list]
     num_configs = len(num_nodes_list)*len(num_items_per_city_list)
-    for epoch in range(last_epoch, args.max_epoch):
+    for epoch in tqdm(range(last_epoch, args.max_epoch), desc="epoch"):
         config_it = epoch%num_configs
         if config_it == 0:
             random.shuffle(config_list)

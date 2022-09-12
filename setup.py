5f30595e8a8be6c20713f79bc3766b8ea457cb34
gemsanyu <git.gemilang.santiyuda@gmail.com>
2022-09-12 16:08:15 +0700

    trans

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ from torch.utils.tensorboard import SummaryWriter
 from torch.utils.data import DataLoader
 
 from agent.agent import Agent
+from transformer.agent import Agent as AgentTrans
 from agent.phn import PHN
 from policy.snes import SNES
 from solver import EPOSolver
@@ -76,7 +77,7 @@ def setup_phn(args):
     phn = PHN(ray_hidden_size=args.ray_hidden_size, 
             num_neurons=args.encoder_size, 
             device=args.device)
-    phn_opt = torch.optim.AdamW(phn.parameters(), lr=1e-4)
+    phn_opt = torch.optim.AdamW(phn.parameters(), lr=args.lr)
     n_params = 0
     for p in phn.parameters():
         n_params += p.numel()
@@ -129,7 +130,53 @@ def setup(args):
                   pointer_num_neurons=args.encoder_size,
                   dropout=args.dropout,
                   n_glimpses=args.n_glimpses)   
-    agent_opt = torch.optim.AdamW(agent.parameters(), lr=1e-4)
+    agent_opt = torch.optim.AdamW(agent.parameters(), lr=args.lr)
+    summary_root = "runs"
+    summary_dir = pathlib.Path(".")/summary_root
+    model_summary_dir = summary_dir/args.title
+    model_summary_dir.mkdir(parents=True, exist_ok=True)
+    writer = SummaryWriter(log_dir=model_summary_dir.absolute())
+
+    checkpoint_root = "checkpoints"
+    checkpoint_dir = pathlib.Path(".")/checkpoint_root/args.title
+    checkpoint_dir.mkdir(parents=True, exist_ok=True)
+    checkpoint_path = checkpoint_dir/(args.title+".pt")
+
+    checkpoint = None
+    if os.path.isfile(checkpoint_path.absolute()):
+        checkpoint = torch.load(checkpoint_path.absolute())
+    else:
+        print("CHECKPOINT NOT FOUND! new run?")
+
+    last_epoch = 0
+    last_step = 0
+    if checkpoint is not None:
+        agent.load_state_dict(checkpoint["agent_state_dict"])
+        agent_opt.load_state_dict(checkpoint["agent_opt_state_dict"])
+        last_epoch = checkpoint["epoch"]
+
+    test_dataset = TTPDataset(dataset_name=args.dataset_name)
+    test_dataloader = DataLoader(test_dataset, batch_size=1)
+    test_batch = next(iter(test_dataloader))
+    coords, norm_coords, W, norm_W, profits, norm_profits, weights, norm_weights, min_v, max_v, max_cap, renting_rate, item_city_idx, item_city_mask, best_profit_kp, best_route_length_tsp = test_batch
+    test_env = TTPEnv(coords, norm_coords, W, norm_W, profits, norm_profits, weights, norm_weights, min_v, max_v, max_cap, renting_rate, item_city_idx, item_city_mask, best_profit_kp, best_route_length_tsp)
+        
+
+    return agent, agent_opt, last_epoch, writer, checkpoint_path, test_env
+
+def setup_transformer(args):
+    # similar to Attention learn routing default
+    agent = AgentTrans(n_heads=8,
+                 num_static_features=3,
+                 num_dynamic_features=4,
+                 n_gae_layers=2,
+                #  embed_dim=128,
+                 embed_dim=64,
+                 gae_ff_hidden=64,
+                #  gae_ff_hidden=512,
+                 tanh_clip=10,
+                 device=args.device)    
+    agent_opt = torch.optim.AdamW(agent.parameters(), lr=args.lr)
     summary_root = "runs"
     summary_dir = pathlib.Path(".")/summary_root
     model_summary_dir = summary_dir/args.title

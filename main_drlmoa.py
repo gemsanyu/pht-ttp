7c751dbeb15e09ad194edd8779b5af99b81c4884
gemsanyu <git.gemilang.santiyuda@gmail.com>
2022-09-21 15:10:57 +0700

    50 agents saja

--- main_drlmoa.py
+++ main_drlmoa.py
@@ -97,7 +97,7 @@ def run(args):
         train_one_epoch(agent, agent_opt, train_dataset, writer, ray)
         # validation_cost = validation_one_epoch(agent, validation_dataset, writer)
         # test_one_epoch(agent, test_env, writer)
-        save(agent, agent_opt, validation_cost, epoch, checkpoint_path)
+        save(agent, agent_opt, 0, epoch, checkpoint_path)
 
 if __name__ == '__main__':
     args = prepare_args()

4060cfc9c21ab36a4e0bdd1d603462b409544f62
gemsanyu <git.gemilang.santiyuda@gmail.com>
2022-11-26 23:28:32 +0800

    try in efison, test

--- test.py
+++ test.py
@@ -57,8 +57,8 @@ def run(args):
 
 if __name__ == '__main__':
     args = prepare_args()
-    # torch.set_num_threads(os.cpu_count())
-    torch.set_num_threads(16)
+    torch.set_num_threads(os.cpu_count()-4)
+    # torch.set_num_threads(16)
     torch.manual_seed(args.seed)
     random.seed(args.seed)
     np.random.seed(args.seed)

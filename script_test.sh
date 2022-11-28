198913ab7711bf5fd3602d731b8c70cf06d59538
gemsanyu <git.gemilang.santiyuda@gmail.com>
2022-11-28 13:50:33 +0800

    test di efison gas

--- script_test.sh
+++ script_test.sh
@@ -1,36 +1,17 @@
-#!/bin/bash
-#
-#SBATCH --job-name=testing-mpn-r1nes
-#SBATCH --output=logs/mpn-r1nes_%A.out
-#SBATCH --error=logs/mpn-r1nes_%A.err
-#
-#SBATCH --time=30-00:00:00
-#SBATCH --nodelist=komputasi09
+#!/bin/bash                      
+
+#SBATCH --ntasks=64         # Contoh menggunakan 32 core CPU.
+#SBATCH --mem=80GB               # Contoh menggunakan RAM 16GB.
+#SBATCH --time=24:00:00          # Contoh menetapkan walltime maks 30 menit.
+#SBATCH --output=logs/result-%j.out   # Output terminal program.
+#SBATCH --error=logs/result-%j.err    # Output verbose program.
+#SBATCH --nodelist=epyc003
 
 source ~/miniconda3/etc/profile.d/conda.sh
-conda activate ttp
+conda activate torch-cpu
 
-python test.py --title att-r1nes  --dataset-name a280-n279 --encoder-size 128 &
-python test.py --title att-r1nesv1  --dataset-name a280-n279 --encoder-size 128 &
-python test.py --title att-r1nesv2  --dataset-name a280-n279 --encoder-size 128 &
-wait;
-
-python test.py --title att-r1nes  --dataset-name a280-n1395 --encoder-size 128 &
-python test.py --title att-r1nesv1  --dataset-name a280-n1395 --encoder-size 128 &
-python test.py --title att-r1nesv2  --dataset-name a280-n1395 --encoder-size 128 &
-wait;
-
-python test.py --title att-r1nes  --dataset-name a280-n2790 --encoder-size 128 &
-python test.py --title att-r1nesv1  --dataset-name a280-n2790 --encoder-size 128 &
-python test.py --title att-r1nesv2  --dataset-name a280-n2790 --encoder-size 128 &
-wait;
-
-python test.py --title att-r1nes  --dataset-name fnl4461-n4460 --encoder-size 128 &
-python test.py --title att-r1nesv1  --dataset-name fnl4461-n4460 --encoder-size 128 &
-python test.py --title att-r1nesv2  --dataset-name fnl4461-n4460 --encoder-size 128 &
-wait;
-
-python test.py --title att-r1nes  --dataset-name fnl4461-n22300 --encoder-size 128 &
-python test.py --title att-r1nesv1  --dataset-name fnl4461-n22300 --encoder-size 128 &
-python test.py --title att-r1nesv2  --dataset-name fnl4461-n22300 --encoder-size 128 &
-wait;
+python test.py --title att-1nes --dataset-name a280-n279;
+python test.py --title att-1nes --dataset-name a280-n1395;
+python test.py --title att-1nes --dataset-name a280-n2790;
+python test.py --title att-1nes --dataset-name fnl4461-n4460;
+python test.py --title att-1nes --dataset-name fnl4461-n22300;

b402eb5f5257ccdf2891a80fc37660912d6dec4f
gemsanyu <git.gemilang.santiyuda@gmail.com>
2022-12-05 11:58:05 +0800

    test profile memory

--- test.py
+++ test.py
@@ -20,34 +20,27 @@ def prepare_args():
     return args
 
 @torch.no_grad()
-def test(agent, phn, test_env, x_file, y_file, n_solutions=10):
+def test(agent, phn, test_env, x_file, y_file, prof, n_solutions=10):
     agent.eval()
     phn.eval()
     ray_list = [torch.tensor([[float(i)/n_solutions,1-float(i)/n_solutions]]) for i in range(n_solutions)]
-    with torch.profiler.profile(
-        schedule=torch.profiler.schedule(wait=1, warmup=1, active=3, repeat=2),
-        on_trace_ready=torch.profiler.tensorboard_trace_handler('./runs/profiler/'),
-        record_shapes=True,
-        profile_memory=True,
-        with_stack=True
-    ) as prof:
-        for ray in tqdm(ray_list, desc="Testing"):
-            param_dict = phn(ray.to(agent.device))
-            tour_list, item_selection, tour_length, total_profit, total_cost, logprob, sum_entropies = solve_cpu_encoder(agent, test_env, param_dict)
-            node_order_str = ""
-            for i in tour_list[0]:
-                node_order_str+= str(i.item()) + " "
-            x_file.write(node_order_str+"\n")
-            item_selection_str = ""
-            for i in item_selection[0]:
-                item_selection_str += (str(int(i.item()))) + " "
-            x_file.write(item_selection_str+"\n")
+    for ray in tqdm(ray_list, desc="Testing"):
+        param_dict = phn(ray.to(agent.device))
+        tour_list, item_selection, tour_length, total_profit, total_cost, logprob, sum_entropies = solve_cpu_encoder(agent, test_env, param_dict)
+        node_order_str = ""
+        for i in tour_list[0]:
+            node_order_str+= str(i.item()) + " "
+        x_file.write(node_order_str+"\n")
+        item_selection_str = ""
+        for i in item_selection[0]:
+            item_selection_str += (str(int(i.item()))) + " "
+        x_file.write(item_selection_str+"\n")
 
-            tour_length = "{:.16f}".format(tour_length[0].item())
-            total_profit = "{:.16f}".format(total_profit[0].item())
-            y_file.write(tour_length+" "+total_profit+"\n")
-            print(tour_length+" "+total_profit+"\n")  
-            prof.step()
+        tour_length = "{:.16f}".format(tour_length[0].item())
+        total_profit = "{:.16f}".format(total_profit[0].item())
+        y_file.write(tour_length+" "+total_profit+"\n")
+        print(tour_length+" "+total_profit+"\n")  
+        prof.step()
 
 def run(args):
     agent, phn, phn_opt, last_epoch, writer, checkpoint_path, test_env, test_sample_solutions = setup_phn(args)
@@ -57,9 +50,14 @@ def run(args):
     model_result_dir.mkdir(parents=True, exist_ok=True)
     x_file_path = model_result_dir/(args.title+"_"+args.dataset_name+".x")
     y_file_path = model_result_dir/(args.title+"_"+args.dataset_name+".f")
-    
-    with open(x_file_path.absolute(), "a+") as x_file, open(y_file_path.absolute(), "a+") as y_file:
-        test(agent, phn, test_env, x_file, y_file)
+    with torch.profiler.profile(
+        schedule=torch.profiler.schedule(wait=1, warmup=1, active=3, repeat=2),
+        on_trace_ready=torch.profiler.tensorboard_trace_handler('./runs/profiler_old/'),
+        record_shapes=True,
+        profile_memory=True,
+        with_stack=True
+    ) as prof, open(x_file_path.absolute(), "a+") as x_file, open(y_file_path.absolute(), "a+") as y_file:
+        test(agent, phn, test_env, x_file, y_file, prof)
 
 if __name__ == '__main__':
     # torch.backends.cudnn.enabled = False

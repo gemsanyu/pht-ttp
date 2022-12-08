067154c75f4ae6e98151f99b4d2d8ebf8300bc94
gemsanyu <git.gemilang.santiyuda@gmail.com>
2022-12-08 10:35:01 +0700

    teted pure

--- script_test.sh
+++ script_test.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-python test.py --title mpn-phn --device cuda --dataset-name a280-n279 --encoder-size 128 &
-python test.py --title mpn-phn-rs64 --ray-hidden-size 64 --device cuda --dataset-name a280-n279 --encoder-size 128 &
-python test.py --title mpn-phn-rs256 --ray-hidden-size 256 --device cuda --dataset-name a280-n279 --encoder-size 128 &
-wait;
-python test.py --title mpn-phn --device cuda --dataset-name a280-n1395 --encoder-size 128 &
-python test.py --title mpn-phn-rs64 --ray-hidden-size 64 --device cuda --dataset-name a280-n1395 --encoder-size 128 &
-python test.py --title mpn-phn-rs256 --ray-hidden-size 256 --device cuda --dataset-name a280-n1395 --encoder-size 128 &
-wait;
-python test.py --title mpn-phn --device cuda --dataset-name a280-n2790 --encoder-size 128 &
-python test.py --title mpn-phn-rs64 --ray-hidden-size 64 --device cuda --dataset-name a280-n2790 --encoder-size 128 &
-python test.py --title mpn-phn-rs256 --ray-hidden-size 256 --device cuda --dataset-name a280-n2790 --encoder-size 128 &
-wait;
-python test.py --title mpn-phn --device cuda --dataset-name fnl4461-n4460 --encoder-size 128 &
-python test.py --title mpn-phn-rs64 --ray-hidden-size 64 --device cuda --dataset-name fnl4461-n4460 --encoder-size 128 &
-python test.py --title mpn-phn-rs256 --ray-hidden-size 256 --device cuda --dataset-name fnl4461-n4460 --encoder-size 128 &
-wait;
-python test.py --title mpn-phn --device cuda --dataset-name fnl4461-n22300 --encoder-size 128 &
-python test.py --title mpn-phn-rs64 --ray-hidden-size 64 --device cuda --dataset-name fnl4461-n22300 --encoder-size 128 &
-python test.py --title mpn-phn-rs256 --ray-hidden-size 256 --device cuda --dataset-name fnl4461-n22300 --encoder-size 128 &
-wait;
+# python test.py --title mpn-phn --device cuda --dataset-name a280-n279 --encoder-size 128 &
+# python test.py --title mpn-phn-rs64 --ray-hidden-size 64 --device cuda --dataset-name a280-n279 --encoder-size 128 &
+# python test.py --title mpn-phn-rs256 --ray-hidden-size 256 --device cuda --dataset-name a280-n279 --encoder-size 128 &
+# wait;
+# python test.py --title mpn-phn --device cuda --dataset-name a280-n1395 --encoder-size 128 &
+# python test.py --title mpn-phn-rs64 --ray-hidden-size 64 --device cuda --dataset-name a280-n1395 --encoder-size 128 &
+# python test.py --title mpn-phn-rs256 --ray-hidden-size 256 --device cuda --dataset-name a280-n1395 --encoder-size 128 &
+# wait;
+# python test.py --title mpn-phn --device cuda --dataset-name a280-n2790 --encoder-size 128 &
+# python test.py --title mpn-phn-rs64 --ray-hidden-size 64 --device cuda --dataset-name a280-n2790 --encoder-size 128 &
+# python test.py --title mpn-phn-rs256 --ray-hidden-size 256 --device cuda --dataset-name a280-n2790 --encoder-size 128 &
+# wait;
+# python test.py --title mpn-phn --device cuda --dataset-name fnl4461-n4460 --encoder-size 128 &
+# python test.py --title mpn-phn-rs64 --ray-hidden-size 64 --device cuda --dataset-name fnl4461-n4460 --encoder-size 128 &
+# python test.py --title mpn-phn-rs256 --ray-hidden-size 256 --device cuda --dataset-name fnl4461-n4460 --encoder-size 128 &
+# wait;
+# python test.py --title mpn-phn --device cuda --dataset-name fnl4461-n22300 --encoder-size 128 &
+# python test.py --title mpn-phn-rs64 --ray-hidden-size 64 --device cuda --dataset-name fnl4461-n22300 --encoder-size 128 &
+# python test.py --title mpn-phn-rs256 --ray-hidden-size 256 --device cuda --dataset-name fnl4461-n22300 --encoder-size 128 &
+# wait;
+python test.py --title mpn-phn_pure --device cuda --dataset-name a280-n279 --encoder-size 128;
+python test.py --title mpn-phn_pure --device cuda --dataset-name a280-n1395 --encoder-size 128;
+python test.py --title mpn-phn_pure --device cuda --dataset-name a280-n2790 --encoder-size 128;
+python test.py --title mpn-phn_pure --device cuda --dataset-name fnl4461-n4460 --encoder-size 128;
+python test.py --title mpn-phn_pure --device cuda --dataset-name fnl4461-n22300 --encoder-size 128;
+python test.py --title mpn-phn_pure --device cuda --dataset-name fnl4461-n44600 --encoder-size 128;

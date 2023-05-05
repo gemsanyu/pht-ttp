cf9222145b76b2f6499bccbdd2a168c2ce802da9
gemilanghpc <git.gemilang.santiyuda@gmail.com>
2023-05-05 14:53:14 +0700

    trained new single agent

--- main_single.py
+++ main_single.py
@@ -144,8 +144,8 @@ def run(args):
     patience=20
     not_improving_count = 0
     agent, agent_opt, critic, crit_total_cost_list, last_epoch, writer, test_env = setup(args)
-    nn_list = [20, 30]
-    nipc_list = [1,3,5]
+    nn_list = [10, 20, 30, 40, 50]
+    nipc_list = [1,3,5,10]
     len_types = len(nn_list)*len(nipc_list)
     train_num_samples_per_dataset = int(args.num_training_samples/len_types)
     validation_num_samples_per_dataset = int(args.num_validation_samples/len_types)

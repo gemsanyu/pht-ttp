70997bac42fbcf59047ce507846bdeaee3610f48
gemilang <git.gemilang.santiyuda@gmail.com>
2024-03-26 10:26:37 +0800

    ready train phn nrw

--- main_phn_sequential_validation.py
+++ main_phn_sequential_validation.py
@@ -191,13 +191,13 @@ def run(args):
     patience = 30
     not_improving_count = 0
     agent, phn, phn_opt, critic_phn, critic_solution_list, training_nondom_list, validation_nondom_list, last_epoch, writer, test_batch, test_sample_solutions = setup_phn(args)
-    nn_list = [10,20,30]
+    nn_list = [20,30,50]
     nipc_list = [1,3,5]
     len_types = len(nn_list)*len(nipc_list)
     train_num_samples_per_dataset = int(args.num_training_samples/len_types)
     validation_num_samples_per_dataset = int(args.num_validation_samples/len_types)
-    training_dataset_list = get_dataset_list(train_num_samples_per_dataset, nn_list, nipc_list, mode="training")
-    validation_dataset_list = get_dataset_list(validation_num_samples_per_dataset, nn_list, nipc_list, mode="validation")
+    training_dataset_list = get_dataset_list(train_num_samples_per_dataset, nn_list, nipc_list, mode="training_nrw")
+    validation_dataset_list = get_dataset_list(validation_num_samples_per_dataset, nn_list, nipc_list, mode="validation_nrw")
 
     
     if last_epoch == 0:
@@ -222,7 +222,7 @@ def run(args):
 
 if __name__ == '__main__':
     args = prepare_args()
-    torch.set_num_threads(1)
+    torch.set_num_threads(16)
     torch.manual_seed(args.seed)
     random.seed(args.seed)
     np.random.seed(args.seed)
